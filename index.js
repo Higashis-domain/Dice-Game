74345a270edff534c2b97fbc4a7990fbe9079bd6
Higashis-domain <important.poorab@gmail.com>
2024-06-20 16:52:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var dice1 = (Math.floor(Math.random() * 6)) + 1;
+var dice2 = (Math.floor(Math.random() * 6)) + 1;
+document.getElementById("dice1").setAttribute("src", "./assets/images/dice" + dice1 + ".png");
+document.getElementById("dice2").setAttribute("src", "./assets/images/dice" + dice2 + ".png");
+if (dice1 > dice2) {
+    document.getElementById("announcer-p").innerHTML = "Player 1 wins";
+} else if (dice1 < dice2) {
+    document.getElementById("announcer-p").innerHTML = "Player 2 wins";
+} else {
+    document.getElementById("announcer-p").innerHTML = "Draw!";
+}
